824880a5bc75709d7045cc56bb1aa236db46c3ca
hospitaler17 <hospitaler17@gmail.com>
2024-10-02 00:24:58 +0300

    Даунгрейд OLED библиотеки до U8glib

--- src/OLED/OLED.h
+++ src/OLED/OLED.h
@@ -1,15 +1,16 @@
 #pragma once
 
-#include <U8g2lib.h>
+#include <Arduino.h>
+#include <U8glib.h>
 #include "../Sensors/SensorData.h"
 
 // SSD1306 OLED DEBUG PRINT
-#define DEBUG_TEXT_STRING_0 00
-#define DEBUG_TEXT_STRING_1 10
-#define DEBUG_TEXT_STRING_2 20
-#define DEBUG_TEXT_STRING_3 30
-#define DEBUG_TEXT_STRING_4 40
-#define DEBUG_TEXT_STRING_5 50
+#define DEBUG_TEXT_STRING_0 10
+#define DEBUG_TEXT_STRING_1 20
+#define DEBUG_TEXT_STRING_2 30
+#define DEBUG_TEXT_STRING_3 40
+#define DEBUG_TEXT_STRING_4 50
+#define DEBUG_TEXT_STRING_5 60
 #define DEBUG_TEXT_FONT u8g_font_6x10
 //
 
@@ -32,5 +33,5 @@ class OLED
     private:
         /// @brief Экземпляр дисплея U8G2 для SSD1306 с разрешением 128x64, без маркировки, с использованием аппаратного I2C.
         /// @note Если динамической памяти будет не хватать, можно заменить U8G2_SSD1306_128X64_NONAME_1_HW_I2C
-        U8G2_SSD1306_128X64_NONAME_2_HW_I2C _u8g2;
+        U8GLIB_SSD1306_128X64 _u8g;
 };
